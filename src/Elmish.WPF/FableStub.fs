b00f2eb63c5cf49726216f99024e889a64006661
Justin <justin.sacks@prolucid.ca>
2017-06-12 09:01:55 -0400

    Adding elmish as a submodule and stubbing out fable dependencies

--- src/Elmish.WPF/FableStub.fs
+++ src/Elmish.WPF/FableStub.fs
@@ -1,9 +1,25 @@
 ﻿[<AutoOpen>]
 module Fable
     [<AutoOpen>]
+    module PowerPack = ()
+    [<AutoOpen>]
+    module Core = 
+        [<AutoOpen>] 
+        module JsInterop = ()
+    [<AutoOpen>]
     module Import =
         [<AutoOpen>] 
+        module Browser =
+            [<AutoOpen>]
+            module console =
+                let error (str,ex) = printfn "%s: %O" str ex
+                let log o = printfn "%A" o
+                let toJson o = o
+        [<AutoOpen>] 
         module JS =
+            [<AutoOpen>]
+            module JSON =
+                let parse str = str
             type Promise<'T>() = 
                 class end
                 with
