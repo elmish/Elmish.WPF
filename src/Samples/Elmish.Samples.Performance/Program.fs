2bc8474ed3ee3b0c72eb2a909224c3ae51447394
Justin <justin.sacks@prolucid.ca>
2017-10-13 16:02:45 -0400

    Fixed #21

--- src/Samples/Elmish.Samples.Performance/Program.fs
+++ src/Samples/Elmish.Samples.Performance/Program.fs
@@ -16,6 +16,7 @@ module Types =
 
     type Msg =
         | AddItems
+        | RemoveItem of Guid
         | NumberToText of Guid
         | ItemMsg of ItemMsg
 
@@ -26,7 +27,7 @@ module Types =
 module State =
     open Types
      
-    let init() = { Items = [] }
+    let init() = { Items = [] }, Cmd.none
 
     let numberToText item = { item with Text = sprintf "Number = %d" item.Number }
 
@@ -41,9 +42,10 @@ module State =
             let newItems = 
                 [ model.Items.Length .. model.Items.Length + numToAdd ]
                 |> List.map (fun i -> { Id = Guid.NewGuid(); Text = "Nothing yet"; Number = i })
-            { model with Items = newItems |> List.append model.Items }
-        | NumberToText id -> { model with Items = model.Items |> List.map (fun i -> if i.Id = id then numberToText i else i)}
-        | ItemMsg msg -> { model with Items = model.Items |> List.map (fun m -> itemUpdate msg m) }
+            { model with Items = newItems |> List.append model.Items }, Cmd.none
+        | RemoveItem id -> { model with Items = model.Items |> List.filter (fun i -> i.Id <> id) }, Cmd.none
+        | NumberToText id -> { model with Items = model.Items |> List.map (fun i -> if i.Id = id then numberToText i else i)}, Cmd.none
+        | ItemMsg msg -> { model with Items = model.Items |> List.map (fun m -> itemUpdate msg m) }, Cmd.none
         
 
 module App =
@@ -51,17 +53,20 @@ module App =
     open Types
     open System.Windows
 
+    let remove i = RemoveItem i.Id
+
     let view _ _ = 
         [ "ArrayItems" |> Binding.oneWay (fun m -> m.Items)
           "ListItems" |> Binding.oneWay (fun m -> m.Items)
           "AddItems" |> Binding.cmd (fun _ _ -> AddItems)
+          "RemoveItem" |> Binding.cmdIf (fun p _ -> p :?> ItemModel |> remove ) (fun p _ -> p <> null)
           "NumItems" |> Binding.oneWayMap (fun m -> m.Items.Length) (sprintf "%d items!")
           "NumToText" |> Binding.cmd (fun p m -> console.log(p); p :?> Guid |> NumberToText)
           "ItemNumToText" |> Binding.cmd (fun p m -> console.log(p); p :?> Guid |> NumberToText) ]
 
     [<EntryPoint;STAThread>]
     let main argv = 
-        Program.mkSimple init update view
+        Program.mkProgram init update view
         |> Program.withExceptionHandler (fun (_, ex) -> MessageBox.Show(ex.Message) |> ignore)
 //        |> Program.withConsoleTrace
 //        |> Program.withSubscription subscribe
