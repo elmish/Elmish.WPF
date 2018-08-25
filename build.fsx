9349fed943479979207210d1838ba63274f4b76f
Christer van der Meeren <cmeeren@protonmail.com>
2018-08-25 16:41:33 +0200

    Add basic FAKE script/setup

---
+++ build.fsx
@@ -0,0 +1,29 @@
+#r "paket: groupref Build //"
+#load ".fake/build.fsx/intellisense.fsx"
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+Target.create "Clean" (fun _ ->
+    !! "src/**/bin"
+    ++ "src/**/obj"
+    |> Shell.cleanDirs 
+)
+
+Target.create "Build" (fun _ ->
+    // Building XAML projects (such as the sample view projects) doesn't work
+    // yet using DotNet.build, so we skip those.
+    !! "src/Elmish.WPF/*.*proj"
+    |> Seq.iter (DotNet.build id)
+)
+
+Target.create "All" ignore
+
+"Clean"
+  ==> "Build"
+  ==> "All"
+
+Target.runOrDefault "All"
