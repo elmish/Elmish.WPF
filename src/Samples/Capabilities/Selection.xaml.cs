73ddad8829ca7c5ac8f9bd2a765337bb1e54fb3b
Tyson Williams <tyson.d.w@gmail.com>
2021-07-22 14:29:34 -0500

    added hyperlink to SelectedIndex sample

--- src/Samples/Capabilities/Selection.xaml.cs
+++ src/Samples/Capabilities/Selection.xaml.cs
@@ -1,9 +1,17 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace Elmish.WPF.Samples.Capabilities {
   public partial class Selection : UserControl {
     public Selection() {
       InitializeComponent();
     }
+
+    private void Hyperlink_RequestNavigate(object _, RequestNavigateEventArgs e) {
+      _ = Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+      e.Handled = true;
+    }
+
   }
 }
