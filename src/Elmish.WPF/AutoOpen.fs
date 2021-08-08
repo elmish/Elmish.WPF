585ef8d1a671135223aaec1c33391488544893d6
Tyson Williams <tyson.d.w@gmail.com>
2021-08-08 10:48:06 -0500

    added DebuggerStepThrough attribute to internal function

--- src/Elmish.WPF/AutoOpen.fs
+++ src/Elmish.WPF/AutoOpen.fs
@@ -2,6 +2,7 @@
 module internal AutoOpen
 
 open System.Collections.Generic
+open System.Diagnostics
 
 
 let flip f b a = f a b
@@ -9,5 +10,6 @@ let flip f b a = f a b
 let ignore2 _ _ = ()
 
 /// Deconstructs a KeyValuePair into a tuple.
+[<DebuggerStepThrough>]
 let (|Kvp|) (kvp: KeyValuePair<_,_>) =
   Kvp (kvp.Key, kvp.Value)
