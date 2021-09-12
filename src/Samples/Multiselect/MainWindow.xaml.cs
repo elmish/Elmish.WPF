d4df2660d0a70c9e5281938ff7118143cf5ed7c4
Tyson Williams <tyson.d.w@gmail.com>
2021-09-12 07:11:13 -0500

    clean code in two App.xaml files

--- src/Samples/Multiselect/MainWindow.xaml.cs
+++ src/Samples/Multiselect/MainWindow.xaml.cs
@@ -1,15 +1,9 @@
 ﻿using System.Windows;
 
-namespace Elmish.WPF.Samples.Multiselect
-{
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+namespace Elmish.WPF.Samples.Multiselect {
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
     }
+  }
 }
