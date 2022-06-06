225a4c3573fabd06737293e0747261c26523e7a4
Joshua Marner <biz@jmarner.com>
2022-06-06 12:54:03 -0500

    Expand OptionalBuilder so that more statements work

--- src/Elmish.WPF/AutoOpen.fs
+++ src/Elmish.WPF/AutoOpen.fs
@@ -14,14 +14,46 @@ let ignore2 _ _ = ()
 let (|Kvp|) (kvp: KeyValuePair<_,_>) =
   Kvp (kvp.Key, kvp.Value)
 
-  
-[<Struct>]
-type OptionalBuilder =
+
+type OptionalBuilder() =
   member _.Bind(ma, f) =
     ma |> Option.bind f
   member _.Return(a) =
     Some a
   member _.ReturnFrom(ma) =
     ma
-    
+  member _.Delay(f) =
+    f
+  member _.Run(f) =
+    f ()
+  member this.Zero() =
+    this.Return()
+  member this.While(guard, body) =
+    if not (guard ()) then this.Zero() else this.Bind(body (), (fun () -> this.While(guard, body)))
+  member this.TryWith(body, handler) =
+    try
+      this.ReturnFrom(body ())
+    with
+    | e -> handler e
+  member this.TryFinally(body, compensation) =
+    try
+      this.ReturnFrom(body ())
+    finally
+      compensation ()
+  member this.Using(disposable: #System.IDisposable, body) =
+    let body' = fun () -> body disposable
+
+    this.TryFinally(
+      body',
+      fun () ->
+        match disposable with
+        | null -> ()
+        | disp -> disp.Dispose()
+    )
+  member this.For(sequence: seq<_>, body) =
+    this.Using(
+      sequence.GetEnumerator(),
+      fun enum -> this.While(enum.MoveNext, this.Delay(fun () -> body enum.Current))
+    )
+
 let option = OptionalBuilder()
