2e1afbe08946a76100b350bdcf7589350500178c
Joshua Marner <biz@jmarner.com>
2022-06-20 13:20:41 -0500

    Update test to use MapOutputType

--- src/Elmish.WPF.Tests/BindingVmHelpersTests.fs
+++ src/Elmish.WPF.Tests/BindingVmHelpersTests.fs
@@ -112,7 +112,6 @@ module Update =
 
       let binding =
         BindingData.TwoWay.id
-        |> BindingData.boxT
 
       let dispatch msg =
         failwith $"Should not dispatch, got {msg}"
@@ -122,7 +121,7 @@ module Update =
           .Recursive(m1, dispatch, (fun () -> model.Value), binding)
         |> Option.defaultWith (fun () -> failwith $"Could not create VmBinding after passing in BindingData: {binding}")
 
-      let updateResult = Update(LoggingViewModelArgs.none, "Nothing").Recursive(ValueSome m1, (fun () -> model.Value), m2, vmBinding)
+      let updateResult = Update(LoggingViewModelArgs.none, "Nothing").Recursive(ValueSome m1, (fun () -> model.Value), m2, vmBinding |> MapOutputType.recursivecase box unbox)
 
       test <@ updateResult |> List.length = 1 @>
     }
