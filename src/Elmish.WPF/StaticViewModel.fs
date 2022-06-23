8913855ce069ba28d32d4edbdc94b0212864f242
Joshua Marner <biz@jmarner.com>
2022-06-23 18:40:34 -0500

    Split BindingT out

--- src/Elmish.WPF/StaticViewModel.fs
+++ src/Elmish.WPF/StaticViewModel.fs
@@ -1,4 +1,4 @@
-﻿module Elmish.WPF.StaticViewModel
+﻿namespace Elmish.WPF
 
 open System
 open System.Collections.Generic
@@ -144,135 +144,9 @@ type StaticHelper<'model, 'msg>(args: ViewModelArgs<'model, 'msg>, getSender: un
       |> Option.defaultValue []
       |> (fun x -> upcast x)
 
-[<AllowNullLiteral>]
-type ISubModel<'model, 'msg> =
-  abstract StaticHelper: StaticHelper<'model, 'msg>
-
 module StaticHelper =
   let create args getSender = StaticHelper(args, getSender)
 
-module BindingT =
-  let internal createStatic data name = { DataT = data; Name = name }
-  open BindingData
-
-  let internal mapData f binding =
-    { DataT = binding.DataT |> f
-      Name = binding.Name }
-
-  /// Map the model of a binding via a contravariant mapping.
-  let mapModel (f: 'a -> 'b) (binding: BindingT<'b, 'msg, 'vm>) = f |> mapModel |> mapData <| binding
-
-  /// Map the message of a binding with access to the model via a covariant mapping.
-  let mapMsgWithModel (f: 'a -> 'model -> 'b) (binding: BindingT<'model, 'a, 'vm>) = f |> mapMsgWithModel |> mapData <| binding
-
-  /// Map the message of a binding via a covariant mapping.
-  let mapMsg (f: 'a -> 'b) (binding: BindingT<'model, 'a, 'vm>) = f |> mapMsg |> mapData <| binding
-
-  /// Set the message of a binding with access to the model.
-  let SetMsgWithModel (f: 'model -> 'b) (binding: BindingT<'model, 'a, 'vm>) = f |> setMsgWithModel |> mapData <| binding
-
-  /// Set the message of a binding.
-  let setMsg (msg: 'b) (binding: BindingT<'model, 'a, 'vm>) = msg |> setMsg |> mapData <| binding
-
-
-  /// Restrict the binding to models that satisfy the predicate after some model satisfies the predicate.
-  let addSticky (predicate: 'model -> bool) (binding: BindingT<'model, 'msg, 'vm>) = predicate |> addSticky |> mapData <| binding
-
-  /// <summary>
-  ///   Adds caching to the given binding.  The cache holds a single value and
-  ///   is invalidated after the given binding raises the
-  ///   <c>PropertyChanged</c> event.
-  /// </summary>
-  /// <param name="binding">The binding to which caching is added.</param>
-  let addCaching (binding: BindingT<'model, 'msg, 'vm>) : BindingT<'model, 'msg, 'vm> =
-    binding
-    |> mapData addCaching
-
-  /// <summary>
-  ///   Adds validation to the given binding using <c>INotifyDataErrorInfo</c>.
-  /// </summary>
-  /// <param name="validate">Returns the errors associated with the given model.</param>
-  /// <param name="binding">The binding to which validation is added.</param>
-  let addValidation (validate: 'model -> string list) (binding: BindingT<'model, 'msg, 'vm>) : BindingT<'model, 'msg, 'vm> =
-    binding
-    |> (addValidation validate |> mapData)
-
-  /// <summary>
-  ///   Adds laziness to the updating of the given binding. If the models are considered equal,
-  ///   then updating of the given binding is skipped.
-  /// </summary>
-  /// <param name="equals">Updating skipped when this function returns <c>true</c>.</param>
-  /// <param name="binding">The binding to which the laziness is added.</param>
-  let addLazy (equals: 'model -> 'model -> bool) (binding: BindingT<'model, 'msg, 'vm>) : BindingT<'model, 'msg, 'vm> =
-    binding
-    |> (addLazy equals |> mapData)
-
-  /// <summary>
-  ///   Accepts a function that can alter the message stream.
-  ///   Ideally suited for use with Reactive Extensions.
-  ///   <code>
-  ///     open FSharp.Control.Reactive
-  ///     let delay dispatch =
-  ///       let subject = Subject.broadcast
-  ///       let observable = subject :&gt; System.IObservable&lt;_&gt;
-  ///       observable
-  ///       |&gt; Observable.delay (System.TimeSpan.FromSeconds 1.0)
-  ///       |&gt; Observable.subscribe dispatch
-  ///       |&gt; ignore
-  ///       subject.OnNext
-  ///
-  ///     // ...
-  ///
-  ///     binding |&gt; Binding.alterMsgStream delay
-  ///   </code>
-  /// </summary>
-  /// <param name="alteration">The function that will alter the message stream.</param>
-  /// <param name="binding">The binding to which the message stream is altered.</param>
-  let alterMsgStream (alteration: ('b -> unit) -> 'a -> unit) (binding: BindingT<'model, 'a, 'vm>) : BindingT<'model, 'b, 'vm> =
-    binding
-    |> (alterMsgStream alteration |> mapData)
-
-  module OneWay =
-    open BindingData.OneWay
-
-    let opt =
-      create id
-      |> createStatic
-
-
-  module OneWayToSource =
-    open BindingData.OneWayToSource
-    
-    let id =
-      create (fun obj _ -> obj)
-      |> createStatic
-
-  module Cmd =
-    open BindingData.Cmd
-
-    let createWithParam exec canExec autoRequery =
-      createWithParam exec canExec autoRequery
-      |> createStatic
-
-  module SubModel =
-    open BindingData.SubModel
-
-    let opt createVm : StaticBindingT<'bindingModel voption, 'msg, 'viewModel :> ISubModel<'bindingModel, 'msg>> =
-      create id createVm (fun ((vm: #ISubModel<'bindingModel,'msg>),m) -> vm.StaticHelper.UpdateModel(m)) (fun _ m -> m)
-      |> createStatic
-
-
 [<AllowNullLiteral>]
-type InnerExampleViewModel(args) as this =
-  interface ISubModel<String, Int64> with
-    member _.StaticHelper = StaticHelper.create args (fun () -> this)
-    
-[<AllowNullLiteral>]
-type ExampleViewModel(args) as this =
-  let staticHelper = StaticHelper.create args (fun () -> this)
-
-  member _.Model
-    with get() = BindingT.OneWay.opt |> staticHelper.GetValue
-    and set(v) = BindingT.OneWayToSource.id >> BindingT.mapMsg int32<string> |> staticHelper.SetValue(v)
-  member _.Command = BindingT.Cmd.createWithParam (fun _ _ -> ValueNone) (fun _ _ -> true) false |> staticHelper.GetValue
-  member _.SubModel = BindingT.SubModel.opt InnerExampleViewModel >> BindingT.mapModel ValueSome >> BindingT.mapMsg int32 |> staticHelper.GetValue
+type ISubModel<'model, 'msg> =
+  abstract StaticHelper: StaticHelper<'model, 'msg>
