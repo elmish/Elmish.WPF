959029c1028646235d24284e41b565c67c2035d1
Joshua Marner <biz@jmarner.com>
2023-01-30 13:33:46 -0600

    Add a sample project to illustrate threading feature

---
+++ src/Samples/Threading/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+using Elmish.WPF.Samples.Threading;
+
+#nullable enable
+namespace Threading {
+  public partial class App : Application {
+    Thread? ElmishThread { get; set; } = null;
+
+    public App() {
+      this.Activated += StartElmish;
+      this.Exit += StopElmish;
+    }
+
+    private void StopElmish(object? sender, ExitEventArgs e)
+    {
+      Dispatcher.FromThread(ElmishThread)?.InvokeShutdown();
+      ElmishThread?.Join();
+    }
+
+    private void StartElmish(object? _1, EventArgs _2) {
+      this.Activated -= StartElmish;
+      ElmishThread = Program.main(MainWindow);
+    }
+  }
+}
---
+++ src/Samples/Threading/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Elmish.WPF.Samples.Threading
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      System.Threading.Tasks.Task.Delay(5000).Wait();
+    }
+  }
+}
