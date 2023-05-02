d479336d638e2f5bb5f2a92e68492b3176cf6e45
Joshua Marner <biz@jmarner.com>
2023-05-01 21:25:06 -0500

    Add example of very weird behavior when setting view model asynchronously

--- src/Samples/Threading/MainWindow.xaml.cs
+++ src/Samples/Threading/MainWindow.xaml.cs
@@ -13,5 +13,11 @@ namespace Elmish.WPF.Samples.Threading
     {
       System.Threading.Tasks.Task.Delay(5000).Wait();
     }
+
+    private void Button2_Click(object sender, RoutedEventArgs e)
+    {
+      dynamic viewModel = DataContext;
+      System.Threading.Tasks.Task.Delay(5000).ContinueWith(t => viewModel.Message = $"{viewModel.Message}{viewModel.Pings}");
+    }
   }
 }
